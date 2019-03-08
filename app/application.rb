9cf9916919fe06e7c8dac572fe593a153cf81a1f
Matteo Muscas <thatman.j@gmail.com>
2019-03-08 13:30:11 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+ 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path=="/cart"
+      resp.write "You requested the cart"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+ 
+    resp.finish
+  end
+end
